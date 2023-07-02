f31ade46309df31f648dafc193be221682c2f82e
Jerry20025 <113283470+Jerry20025@users.noreply.github.com>
2023-07-02 17:11:02 +0530

    Create subarraysum.cpp

---
+++ subarraysum.cpp
@@ -0,0 +1,14 @@
+int subarraySum(vector<int>& nums, int k) {
+         int sum=0; 
+         unordered_map<int,int>mp; 
+         int ans=0; 
+         mp[0]=1; 
+         for(int i=0;i<nums.size();i++){
+             sum+=nums[i]; 
+             if(mp.find(sum-k)!=mp.end()){
+                 ans+=mp[sum-k]; 
+             }
+             mp[sum]++; 
+         }
+         return ans;
+    }
